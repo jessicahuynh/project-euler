332bc9acc0e95c06333699a897f0075f7fc65939
Jessica Huynh <jhuynh37@brandeis.edu>
2012-07-21 14:52:35 -0500

    finished problem 2

---
+++ problem0002.py
@@ -0,0 +1,16 @@
+﻿# for the terms in the Fibonacci less than 4 million, sum the even terms
+limit = 4000000
+firstTerm = 1
+secondTerm = 1
+evenNumber = firstTerm + secondTerm;
+sum = 0
+
+while (evenNumber < limit):
+	sum += evenNumber
+	
+	# the third number in the sequence is always even
+	firstTerm = secondTerm + evenNumber
+	secondTerm = evenNumber + firstTerm
+	evenNumber = firstTerm + secondTerm
+	
+print(sum)
